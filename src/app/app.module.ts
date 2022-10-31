07ee031ba5ba3abe87639dab299ed5eee4721e1c
Rhaynner Liberato <rhaynnerliberato@MacBook-Pro-de-Rhaynner.local>
2022-10-31 14:50:06 -0300

    carousel

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './componentes/carousel/carousel.component';
+import { CarouselModule } from './componentes/carousel/carousel.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
