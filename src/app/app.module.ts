5f8976f31692e9d79c89deb174e4fc3dc5dabd87
Rhaynner Liberato <rhaynnerliberato@MacBook-Pro-de-Rhaynner.local>
2022-11-01 12:35:07 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './componentes/carousel/carousel.component';
 import { CarouselModule } from './componentes/carousel/carousel.module';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { NavbarModule } from './componentes/navbar/navbar.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule
+    CarouselModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
